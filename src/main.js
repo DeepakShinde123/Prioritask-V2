02ca7b575b8c702d5707f68f3d711a30fc9ad297
DeepakShinde123 <Deepakshinde123@proton.me>
2024-10-28 08:02:15 +0000

    Done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./index.css";
+import { titleCase } from "./utils";
+
+const inputEl = document.querySelector("[data-user-input]");
+const formEl = document.querySelector("[data-form]");
+const tasks = [];
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputEl.value) return;
+  const newTask = {
+    tasks: titleCase(inputEl.value),
+    isCompleted: false,
+    id: tasks.lenght,
+  };
+  tasks.unshift(newTask);
+
+  inputEl.value = "";
+});
+
