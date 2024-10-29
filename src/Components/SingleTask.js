4e314093ca9db1e52f09211704228c314db2cd18
DeepakShinde123 <Deepakshinde123@proton.me>
2024-10-29 07:07:56 +0000

    add listen click on each task

---
+++ src/Components/SingleTask.js
@@ -0,0 +1,13 @@
+export default function (text, isCompleted, id) {
+  const label = document.createElement("label");
+  label.classList.add("label", "cursor-pointer");
+  if (isCompleted) {
+    label.classList.add("bg-slate-200", "rounded", "p-2");
+  }
+  label.innerHTML = `
+    <span class="label-text">${text}</span>
+    <input type="checkbox" ${
+      isCompleted && "checked"
+    } class="checkbox rounded-2xl" id=${id} />`;
+  return label;
+}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,39 @@
 import "./index.css";
+import SingleTask from "./Components/SingleTask";
 import { titleCase } from "./utils";
 
-const inputEl = document.querySelector("[data-user-input]");
 const formEl = document.querySelector("[data-form]");
+const inputEl = document.querySelector("[data-user-input]");
+const taskContainerEl = document.querySelector("[data-task-container]");
+
 const tasks = [];
+
+function renderTask() {
+  taskContainerEl.innerHTML = "";
+  const frag = document.createDocumentFragment();
+  tasks.forEach((task) => {
+    frag.appendChild(SingleTask(task.task, task.isCompleted, task.id));
+  });
+  taskContainerEl.appendChild(frag);
+}
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!inputEl.value) return;
+
   const newTask = {
-    tasks: titleCase(inputEl.value),
-    isCompleted: false,
-    id: tasks.lenght,
+    task: titleCase(inputEl.value),
+    isCompleted: true,
+    id: tasks.length,
   };
+
   tasks.unshift(newTask);
+
+  renderTask();
 
   inputEl.value = "";
 });
 
+taskContainerEl.addEventListener("click", (e) => {
+  console.log(e.target.id);
+});
